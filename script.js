8b9371bac53681a3302d54324c187f309f7f4b44
jswelsh <james.sinclair.welsh@gmail.com>
2020-02-21 10:21:59 -0800

    add query, cast message to dom for load and returned response

--- script.js
+++ script.js
@@ -7,4 +7,14 @@ function getUserLocation() {
   var loadingMessage = document.getElementById('loading-message');
   var returnMessage = document.getElementById('return-message');
   document.getElementById('loading-message').textContent = 'loading';
+
+  function success(position) {
+    var lon = position.coords.longitude;
+    var lat = position.coords.latitude;
+    var query = 'https://api.mapbox.com/v4/examples.4ze9z6tv/tilequery/' + lon + ',' + lat + '.json?access_token=' + accessToken;
+    var loadingMessage = document.getElementById('loading-message');
+    var returnMessage = document.getElementById('return-message');
+    document.getElementById('loading-message').textContent = 'loading';
+
+  }
 }
